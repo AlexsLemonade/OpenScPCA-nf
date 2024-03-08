4b27a91456f628334a212adaeaf3d011c877350a
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-03-08 15:19:29 -0500

    Add terraform config for nextflow

---
+++ terraform/nextflow-batch.tf
@@ -0,0 +1,37 @@
+# AWS Batch setup
+provider "aws" {
+  region = "us-east-2"
+}
+
+variable "default_tags" {
+  description = "Default resource tags"
+  type        = map(string)
+  default = {
+    team    = "science"
+    project = "openscpca"
+    purpose = "openscpca-nf-batch"
+    config  = "https://github.com/AlexsLemonade/OpenScPCA-nf/terraform"
+  }
+
+}
+
+resource "aws_batch_job_queue" "nf_default_queue" {
+  name     = "openscpca-nf-batch-default-queue"
+  tags     = var.default_tags
+  state    = "ENABLED"
+  priority = 1
+  compute_environment_order {
+    order               = 1
+    compute_environment = aws_batch_compute_environment.nf_spot.arn
+  }
+}
+
+resource "aws_batch_job_queue" "nf_priority_queue" {
+  name     = "openscpca-nf-batch-priority-queue"
+  state    = "ENABLED"
+  priority = 1
+  compute_environment_order {
+    order               = 1
+    compute_environment = aws_batch_compute_environment.nf_spot.arn
+  }
+}
---
+++ terraform/nextflow-launch-templates.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_template" "nf_lt_standard" {
+  name = "nextflow-launchtemplate-standard"
+  tags = var.default_tags
+  # the AMI used is the Amazon ECS-optimized Amazon Linux 2023 AMI
+  # id determined with `aws ssm get-parameters --names /aws/service/ecs/optimized-ami/amazon-linux-2023/recommended --region us-east-2`
+  image_id = "ami-06adbea8e9d7cae16"
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = 128 #GiB
+      volume_type           = "gp3"
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
+  update_default_version = true
+}
