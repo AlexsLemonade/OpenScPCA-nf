a93b4b11d8a8fb9f495a19caf229834d2eda95c6
Ally Hawkins <ally.hawkins@ccdatalab.org>
2025-03-05 16:19:15 -0600

    remove calculating proliferative and don't use <<-

--- modules/cell-type-ewings/resources/usr/bin/assign-celltypes.R
+++ modules/cell-type-ewings/resources/usr/bin/assign-celltypes.R
@@ -2,17 +2,15 @@
 
 # This script is used to assign cell type annotations to Ewing sarcoma libraries in SCPCP000015
 # Tumor cells are assigned based on those with AUC values above specified thresholds for specific gene sets
-# Tumor cells are then classified into EWS-high, EWS-low, and EWS-high proliferative
+# Tumor cells are then classified into EWS-high, EWS-low
 # Normal cells are all non-tumor cells and are labeled with the consensus cell type
+# if tumor cells have mean expression of proliferative markers > 0 "proliferative" will be appended to cell type label
+
+# `custom_annotation` and `custom_ontology` columns in the output TSV file will contain the final cell type labels
 
 library(optparse)
 
 option_list <- list(
-  make_option(
-    opt_str = c("--sce_file"),
-    type = "character",
-    help = "Path to RDS file containing a processed SingleCellExperiment object annotate."
-  ),
   make_option(
     opt_str = c("--consensus_celltype_file"),
     type = "character",
@@ -31,12 +29,11 @@ option_list <- list(
     help = "Path to TSV file containing the AUC values to use as thresholds for assigning tumor cell states."
   ),
   make_option(
-    opt_str = c("--cell_state_markers_file"),
+    opt_str = c("--mean_gene_expression_file"),
     type = "character",
     default = NULL,
-    help = "Path to TSV file with marker genes to use for labeling EWS-high proliferative cells.
-      Must contain a `cell_state` and `ensembl_gene_id` column.
-      Marker genes that have `cell_state == proliferative` will be used."
+    help = "Path to TSV file with mean expression of custom marker gene sets.
+      Cells with mean expression of proliferative markers > 0 will be labeled with proliferative."
   ),
   make_option(
     opt_str = c("--output_file"),
@@ -52,35 +49,17 @@ opt <- parse_args(OptionParser(option_list = option_list))
 
 # make sure input files exist
 stopifnot(
-  "sce file does not exist" = file.exists(opt$sce_file),
   "consensus cell types file does not exist" = file.exists(opt$consensus_celltype_file),
   "AUCell results file does not exist" = file.exists(opt$aucell_results_file),
-  "auc_table file does not exist" = file.exists(opt$auc_table),
-  "cell_state_markers_file does not exist" = file.exists(opt$cell_state_markers_file)
+  "auc_thresholds_file file does not exist" = file.exists(opt$auc_thresholds_file),
+  "mean_gene_expression_file does not exist" = file.exists(opt$mean_gene_expression_file)
 )
 
-# load SCE
-suppressPackageStartupMessages({
-  library(SingleCellExperiment)
-})
-
 # read in files
-sce <- readr::read_rds(opt$sce_file)
 consensus_df <- readr::read_tsv(opt$consensus_celltype_file)
 aucell_results_df <- readr::read_tsv(opt$aucell_results_file)
-auc_threshold_df <- readr::read_tsv(opt$auc_table)
-cell_state_markers_df <- readr::read_tsv(opt$cell_state_markers_file)
-
-
-# assign normal cell types using consensus cell types --------------------------
-assignment_df <- consensus_df |>
-  dplyr::mutate(
-    custom_annotation = dplyr::if_else(
-      !is.na(consensus_ontology),
-      consensus_annotation,
-      "unknown"
-    )
-  )
+auc_threshold_df <- readr::read_tsv(opt$auc_thresholds_file)
+mean_exp_df <- readr::read_tsv(opt$mean_gene_expression_file)
 
 
 # assign tumor cells and tumor cell states -------------------------------------
@@ -88,9 +67,8 @@ assignment_df <- consensus_df |>
 tumor_cell_states <- unique(auc_threshold_df$cell_type)
 
 # for each cell state get a list of cells that belong to that cell state using AUCell results
-# then update the existing custom annotation column to label those cells with that cell state
-tumor_cell_states |>
-  purrr::walk(\(state){
+cell_state_df <- tumor_cell_states |>
+  purrr::map(\(state){
     # get a list of all gene sets that should be used to classify that cell state
     geneset_auc_df <- auc_threshold_df |>
       dplyr::filter(cell_type == state) |>
@@ -105,37 +83,48 @@ tumor_cell_states |>
       }) |>
       purrr::reduce(intersect)
 
-    # figure out which cells these are
-    cell_state_idx <- which(assignment_df$barcodes %in% cells)
+    df <- data.frame(
+      barcodes = cells,
+      custom_annotation = state
+    )
+  }) |>
+  purrr::list_rbind() |>
+  dplyr::group_by(barcodes) |>
+  # account for anything that could be in both groups
+  dplyr::summarise(
+    custom_annotation = paste0(custom_annotation, collapse = ",")
+  )
 
-    # label the cells with that cell state
-    # modify the data frame in place with <<-
-    assignment_df$custom_annotation[cell_state_idx] <<- state
-  })
+# add custom annotations back with original annotations
+# any cells that aren't tumor cells will be NA
+assignment_df <- consensus_df |>
+  dplyr::left_join(cell_state_df, by = "barcodes")
+
+# assign normal cell types using consensus cell types --------------------------
+
+assignment_df <- assignment_df |>
+  dplyr::mutate(
+    custom_annotation = dplyr::if_else(
+      is.na(custom_annotation),
+      consensus_annotation, # use consensus labels if not a tumor cell
+      custom_annotation # otherwise keep tumor cell label
+    )
+  )
 
 # Label proliferative tumor cells ----------------------------------------------
 
-# get proliferative marker genes
-proliferative_markers <- cell_state_markers_df |>
-  dplyr::filter(cell_state == "proliferative") |>
-  dplyr::pull(ensembl_gene_id)
-
-# calculate the mean expression of all genes for all cells
-mean_exp <- logcounts(sce[proliferative_markers, ]) |>
-  as.matrix() |>
-  t() |>
-  rowMeans()
-
-# get cells with mean > 0
-proliferative_cells <- names(mean_exp)[mean_exp > 0]
+# get cells with proliferative mean > 0
+proliferative_cells <- mean_exp_df |>
+  dplyr::filter(proliferative_mean_expression > 0) |>
+  dplyr::pull(barcodes)
 
 # create final assignments
 assignment_df <- assignment_df |>
   dplyr::mutate(
-    # assign proliferative based on being EWS-high and mean proliferative > 0
+    # add proliferative label to tumor cells if mean proliferative > 0
     custom_annotation = dplyr::if_else(
-      barcodes %in% proliferative_cells & custom_annotation == "tumor EWS-high",
-      "tumor EWS-high proliferative",
+      barcodes %in% proliferative_cells & stringr::str_detect(custom_annotation, "tumor"),
+      glue::glue("{custom_annotation} proliferative"),
       custom_annotation
     ),
     # add ontology column
