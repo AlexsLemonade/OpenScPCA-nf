6586dd2860deda61b2ece6d80777009991a40e7c
Ally Hawkins <ally.hawkins@ccdatalab.org>
2025-08-25 16:12:49 -0500

    process and script for exporting json

---
+++ modules/export-annotations/main.nf
@@ -0,0 +1,52 @@
+#!/usr/bin/env nextflow
+
+// Workflow to format and export openscpca annotations
+
+process format_annotations {
+  container params.scpcatools_slim_container
+  tag "${sample_id}"
+  label 'mem_8'
+  publishDir "${params.annotations_bucket}/${params.release_prefix}/${project_id}/${sample_id}", mode: 'copy'
+  input:
+    tuple val(sample_id),
+          val(project_id),
+          path(annotations_tsv_files),
+          val(annotation_column),
+          val(ontology_column),
+          val(module_name)
+  output:
+    tuple val(sample_id),
+          val(project_id),
+          path(json_files)
+  script:
+    library_ids = annotations_tsv_files.collect{(it.name =~ /SCPCL\d{6}/)[0]}
+    json_files = library_ids.collect{"${it}_openscpca-annotations.json"}
+    ontology_included = "${ontology_column}" != "NONE"
+    """
+    for library_id in ${library_ids.join(" ")};do
+      # get the input and output files for the library id
+      annotations_file=\$(ls ${annotations_tsv_files} | grep "\${library_id}")
+      json_file=\$(ls ${json_files} | grep "\${library_id}")
+
+      export-celltype-json.R \
+        --annotations_tsv_file \$annotations_file \
+        --annotation_column ${annotation_column} \
+        ${ontology_included ? "--ontology_column  ${ontology_column}" : ''} \
+        --module_name ${module_name} \
+        --release_data ${params.release_prefix} \
+        --openscpca_nf_version ${workflow.manifest.version} \
+        --output_json_file \$json_file
+    done
+    """
+}
+
+workflow export_annotations {
+  take:
+    celltype_ch  // [sample_id, project_id, [cell type assignment files], annotation column, ontology column, module name]
+  main:
+    // export json
+    format_annotations(celltype_ch)
+
+  emit:
+    format_annotations.out // [sample id, project id, annotations json]
+}
---
+++ modules/export-annotations/resources/usr/bin/export-celltype-json.R
@@ -0,0 +1,98 @@
+#!/usr/bin/env Rscript
+
+# This script is used to create a JSON file of annotations for a single library
+# JSON file will include barcodes, annotation column, ontology column (if provided),
+# openscpca-nf version, data release data, and module name
+
+library(optparse)
+
+option_list <- list(
+  make_option(
+    opt_str = c("--annotations_tsv_file"),
+    type = "character",
+    help = "Path to TSV file with cell type annotations"
+  ),
+  make_option(
+    opt_str = c("--annotation_column"),
+    type = "character",
+    help = "Name of the column containing the cell type annotations to use for openscpca_celltype_annotation"
+  ),
+  make_option(
+    opt_str = c("--ontology_column"),
+    default = "",
+    type = "character",
+    help = "Name of the column containing the cell type ontology IDs to use for openscpca_celltype_ontology"
+  ),
+  make_option(
+    opt_str = c("--module_name"),
+    type = "character",
+    help = "Name of original module in OpenScPCA-analysis"
+  ),
+  make_option(
+    opt_str = c("--release_date"),
+    type = "character",
+    help = "Release date of data used when generating annotations"
+  ),
+  make_option(
+    opt_str = c("--openscpca_nf_version"),
+    type = "character",
+    help = "Version of OpenScPCA-nf workflow"
+  ),
+  make_option(
+    opt_str = "--output_json_file",
+    type = "character",
+    help = "Path to JSON file to save cell type annotations"
+  )
+)
+
+# Parse options
+opt <- parse_args(OptionParser(option_list = option_list))
+
+# Set up -----------------------------------------------------------------------
+
+# make sure input/output exist
+stopifnot(
+  "annotations TSV file does not exist" = file.exists(opt$annotations_tsv_file),
+  "annotation column must be provided" = !is.null(opt$annotation_column),
+  "module name must be provided" = !is.null(opt$module_name),
+  "release date must be provided" = !is.null(opt$release_date),
+  "openscpca-nf version must be provided" = !is.null(opt$openscpca_nf_version),
+  "output json file must end in .json" = stringr::str_ends(opt$output_json_file, "\\.json")
+)
+
+# read in annotations
+annotations_df <- readr::read_tsv(opt$annotations_tsv_file)
+
+# check that barcodes and annotation column exist
+stopifnot(
+  "barcodes column must be present in provided TSV file" = "barcodes" %in% colnames(annotations_df),
+  "annotation column is not present in provided TSV file" = opt$annotation_column %in% colnames(annotations_df)
+)
+
+# check for ontology ids if provided
+if (!is.null(opt$ontology_column)) {
+  stopifnot(
+    "ontology column is not present in provided TSV file" = opt$ontology_column %in% colnames(annotations_df)
+  )
+  ontology_ids <- annotations_df[[opt$ontology_column]]
+} else {
+  ontology_ids <- NA
+}
+
+# build json contents
+json_contents <- list(
+  barcodes = annotations_df$barcodes,
+  openscpca_celltype_annotation = annotations_df[[opt$annotation_column]],
+  openscpca_celltype_ontology = ontology_ids,
+  module_name = opt$module_name,
+  openscpca_nf_version = opt$openscpca_nf_version,
+  release_date = opt$release_date
+)
+
+# export json file
+jsonlite::write_json(
+  json_contents,
+  path = opt$output_json_file,
+  auto_unbox = TRUE,
+  pretty = TRUE
+)
